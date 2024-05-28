75ed63372c0a80ea1a4fdbbc0fbe4d919de4cce7
UmayerK <95543241+UmayerK@users.noreply.github.com>
2024-05-28 08:32:29 -0400

    initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+// src/components/Contact.jsx
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl">Contact Us</h1>
+    </div>
+  );
+};
+
+export default Contact;
